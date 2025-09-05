8c9a9f8fa7d38f466389395f7635e0b71ae8a82d
jmmoses99 <jmmoses99@gmail.com>
2025-09-05 15:50:43 -0400

    Updates in Comments
    Updates in Comments for Assignment2

--- R Assignment 2.R
+++ R Assignment 2.R
@@ -1,4 +1,16 @@
 #Assignment 2 Description: Writing Functions and Debugging
+#Use the vector:
+#assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+#Consider this function:
+#myMean <- function(assignment2){
+#return(sum(assignment) / length(someData))
+#}
+#print(myMean(assignment2))
+#The error message after print(myMean(assignment2)) was Object 'assignment' not found
+#called from: myMean(assignment2)
+# The function variables could not be found by R to be pulled into the function
+#The original vector name was assignment2
+
 #Corrected Function and Debug by Me
  assignment2 <- c(16,18,14,22,27,17,19,17,17,22,20,22)
  Mymean <- function(x){

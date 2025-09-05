76ced927df31cbf4cffcc6c7a44573b248cb8fdc
jmmoses99 <jmmoses99@gmail.com>
2025-09-05 15:36:51 -0400

    Update to Corrected Code
    Corrected Code Updates for Assignment 2

--- R Assignment 2.R
+++ R Assignment 2.R
@@ -1,9 +1,7 @@
-#Assignment 2
-#Evaluate myMean Function
-#Use the vector:
-#assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
-#Consider this function:
-#myMean <- function(assignment2) {
-#return(sum(assignment) / length(someData))
-# }
-#In RStudio, run myMean(assignment2) and record the output or error.
+#Assignment 2 Description: Writing Functions and Debugging
+#Corrected Function and Debug by Me
+ assignment2 <- c(16,18,14,22,27,17,19,17,17,22,20,22)
+ Mymean <- function(x){
+   return(sum(assignment2)/length(assignment2))}
+   print(Mymean(x))
+ 

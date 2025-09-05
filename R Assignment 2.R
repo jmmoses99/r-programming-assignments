205daff2239bd8991eb75629fe3f4cd1a99fb839
jmmoses99 <jmmoses99@gmail.com>
2025-09-05 14:55:06 -0400

    Assignment 2
    Description of Assignment 2

---
+++ R Assignment 2.R
@@ -0,0 +1,9 @@
+#Assignment 2
+#Evaluate myMean Function
+#Use the vector:
+#assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+#Consider this function:
+#myMean <- function(assignment2) {
+#return(sum(assignment) / length(someData))
+# }
+#In RStudio, run myMean(assignment2) and record the output or error.
